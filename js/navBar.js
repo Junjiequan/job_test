0c9552341d96993e812759866c6cc0cd81f6152d
mmmG <a331998513@gmail.com>
2021-06-12 22:57:46 +0200

    update final version

--- js/navBar.js
+++ js/navBar.js
@@ -1,19 +1,38 @@
-/**
-data-js-hamburger animation
-**/
-let forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
-
+const mobNavBar = document.querySelector('[data-js="mob-nav-menu"]');
+const hamburgerIcon = document.querySelector('[data-js-hamburger]');
 const hamburgers = document.querySelectorAll(".hamburger");
 
-const navHamburger = () =>{
+// data-js-hamburger animation 
+let forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
 
+const openNavMenu = () =>{
+    hamburgerIcon.classList.toggle("is-active");
+    mobNavBar.style.transform = "translateY(0)";
+}
+const closeNavMenu = () =>{
+    hamburgerIcon.classList.toggle("is-active");
+    mobNavBar.style.transform = "translateY(-100%)";
+}
+
+const navHamburger = () =>{
     if (hamburgers.length > 0) {
         forEach(hamburgers, function(hamburger) {
             hamburger.addEventListener("click", function() {
-            this.classList.toggle("is-active");
-            }, false);
+                !hamburgerIcon.classList.contains('is-active')
+                ? openNavMenu()
+                : closeNavMenu();
+            });
         });
     }
+
+    document.addEventListener('click',(e)=>{
+        const target = e.target;
+        const hamburger = target.closest('.hamburger');
+        if(hamburgerIcon.classList.contains('is-active') && hamburger !== hamburgerIcon)
+            closeNavMenu();
+    })
 }
 
-export {navHamburger};
+
+
+export { navHamburger };
